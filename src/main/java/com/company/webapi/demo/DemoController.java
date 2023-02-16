7ab432b07de51723697d150d0242a92a448d7672
Nicola <nbroll@seac.it>
2023-02-16 14:47:00 +0100

    Init

---
+++ src/main/java/com/company/webapi/demo/DemoController.java
@@ -0,0 +1,29 @@
+package com.company.webapi.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/demo-controller")
+public class DemoController {
+
+	@GetMapping
+	public ResponseEntity<String> sayHello() {
+		return ResponseEntity.ok("Hello from secured endpoint " + getUserLogged());
+	}
+	
+	private String getUserLogged() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (!(authentication instanceof AnonymousAuthenticationToken)) {
+		    String currentUserName = authentication.getName();
+		    return currentUserName;
+		}
+		return null;
+	}
+
+}
---
+++ src/main/java/com/company/webapi/user/Role.java
@@ -0,0 +1,5 @@
+package com.company.webapi.user;
+
+public enum Role {
+	USER, ADMIN
+}
